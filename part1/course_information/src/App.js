054567f62a651a42b94178c621f440d0890086aa
kavanderjanne <kavanderjanne@gmail.com>
2021-08-14 19:08:51 +0300

    1.2

--- part1/course_information/src/App.js
+++ part1/course_information/src/App.js
@@ -11,16 +11,16 @@ const Content = ({
   exercises3,
 }) => (
   <>
-    <p>
-      {part1} {exercises1}
-    </p>
-    <p>
-      {part2} {exercises2}
-    </p>
-    <p>
-      {part3} {exercises3}
-    </p>
+    <Part part={part1} exercises={exercises1} />
+    <Part part={part2} exercises={exercises2} />
+    <Part part={part3} exercises={exercises3} />
   </>
+)
+
+const Part = ({ part, exercises }) => (
+  <p>
+    {part} {exercises}
+  </p>
 )
 
 const Total = ({ exercises1, exercises2, exercises3 }) => (
