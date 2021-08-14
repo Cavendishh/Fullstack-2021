34963f2aa5a5d11412edf6239d109db47fbef9fb
kavanderjanne <kavanderjanne@gmail.com>
2021-08-14 19:59:57 +0300

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,18 +6,40 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [amount, setAmount] = useState(0)
+
+  const onPosFeedback = () => {
+    setGood(good + 1)
+    setAverage(average + 1)
+    setAmount(amount + 1)
+  }
+
+  const onNeuFeedback = () => {
+    setNeutral(neutral + 1)
+    setAmount(amount + 1)
+  }
+
+  const onBadFeedback = () => {
+    setBad(bad + 1)
+    setAverage(average - 1)
+    setAmount(amount + 1)
+  }
 
   return (
     <div>
       <h2>Give feedback</h2>
-      <Button onClick={() => setGood(good + 1)} text={'good'} />
-      <Button onClick={() => setNeutral(neutral + 1)} text={'neutral'} />
-      <Button onClick={() => setBad(bad + 1)} text={'bad'} />
+      <Button onClick={onPosFeedback} text={'good'} />
+      <Button onClick={onNeuFeedback} text={'neutral'} />
+      <Button onClick={onBadFeedback} text={'bad'} />
       <h2>Statistics</h2>
       <ul>
         <li>good {good}</li>
         <li>neutral {neutral}</li>
         <li>bad {bad}</li>
+        <li>all {bad}</li>
+        <li>average {average / amount}</li>
+        <li>positive {(good / amount) * 100} %</li>
       </ul>
     </div>
   )
