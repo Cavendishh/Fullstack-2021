2c2054e22bb7cabaf7f2540c14995c9b31f56c7a
kavanderjanne <kavanderjanne@gmail.com>
2021-08-21 12:29:01 +0300

    4.4

--- part4/blog_list/tests/list_helper.test.js
+++ part4/blog_list/tests/list_helper.test.js
@@ -1,8 +1,24 @@
-const dummy = require('../utils/list_helper').dummy
+const listHelper = require('../utils/list_helper')
+const blogs = require('./dummyBlogs')
 
 test('dummy returns one', () => {
-  const blogs = []
-
-  const result = dummy(blogs)
+  const result = listHelper.dummy([])
   expect(result).toBe(1)
+})
+
+describe('Total likes', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(blogs.one)
+    expect(result).toBe(5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(blogs.all)
+    expect(result).toBe(36)
+  })
 })
