9a8da47956ee4f64481e9e0c5710aedd249fb0c8
kavanderjanne <kavanderjanne@gmail.com>
2021-08-21 17:31:56 +0300

    4.8

---
+++ part4/blog_list/tests/blog_api.test.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all bogs are returned', async () => {
+  const res = await api.get('/api/blogs')
+
+  expect(res.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
--- part4/blog_list/utils/logger.js
+++ part4/blog_list/utils/logger.js
@@ -1,10 +1,6 @@
-const info = (...params) => {
-  console.log(...params)
-}
+const info = (...params) => process.env.NODE_ENV !== 'test' && console.log(...params)
 
-const error = (...params) => {
-  console.error(...params)
-}
+const error = (...params) => process.env.NODE_ENV !== 'test' && console.error(...params)
 
 module.exports = {
   info,
