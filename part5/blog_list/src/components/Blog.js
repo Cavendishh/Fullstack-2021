4d4a13e6c86d2cbac9f3f812d0930e2e28866d98
kavanderjanne <kavanderjanne@gmail.com>
2021-10-10 14:49:18 +0300

    5.7

--- part5/blog_list/src/components/Blog.js
+++ part5/blog_list/src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  console.log(blog)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const fullBlog = () => (
+    <>
+      <p>{blog.url}</p>
+      <p>
+        {blog.likes} <button>like</button>
+      </p>
+      <p>{blog.author}</p>
+    </>
+  )
+
+  return (
+    <div style={blogStyle}>
+      <p>
+        {blog.title}{' '}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? 'Hide' : 'Show'}
+        </button>
+      </p>
+
+      {showDetails && fullBlog()}
+    </div>
+  )
+}
 
 export default Blog
