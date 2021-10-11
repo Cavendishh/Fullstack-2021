ea048daea4a75fd4afe3c8ad6d57542d83d5c9d0
kavanderjanne <kavanderjanne@gmail.com>
2021-10-11 23:40:57 +0300

    5.19

--- part5/blog_list/cypress/integration/blog_app.spec.js
+++ part5/blog_list/cypress/integration/blog_app.spec.js
@@ -4,6 +4,11 @@ describe('Blog app', function () {
     username: 'test',
     password: 'test',
   }
+  const blog = {
+    title: 'New title',
+    author: 'Test author',
+    url: 'www.testurl.fi',
+  }
 
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
@@ -33,6 +38,28 @@ describe('Blog app', function () {
 
       cy.get('.error').should('contain', 'Username or password is invalid')
       cy.get('html').should('not.contain', 'Succesfully logged in')
+    })
+  })
+
+  describe('When logged in', function () {
+    beforeEach(function () {
+      cy.request('POST', 'http://localhost:3003/api/login', {
+        username: user.username,
+        password: user.password,
+      }).then((res) => {
+        localStorage.setItem('loggedUser', JSON.stringify(res.body))
+        cy.visit('http://localhost:3000')
+      })
+    })
+
+    it.only('a blog can be created', function () {
+      cy.contains('Create a blog').click()
+      cy.get('#title-input').type(blog.title)
+      cy.get('#author-input').type(blog.author)
+      cy.get('#url-input').type(blog.url)
+      cy.get('#create-blog-btn').click()
+
+      cy.contains(blog.title)
     })
   })
 })
