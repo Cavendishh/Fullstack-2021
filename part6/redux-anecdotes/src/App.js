55541940d0aa77ccd4eaac05418df6b9edcca1d2
kavanderjanne <kavanderjanne@gmail.com>
2021-10-12 15:18:31 +0300

    6.13

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService.getAll().then((anecdotes) => dispatch(initializeAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <>
       <h2>Anecdotes</h2>
---
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+  const res = await axios.get(baseUrl)
+  return res.data
+}
+
+const exportsObj = {
+  getAll,
+}
+
+export default exportsObj
