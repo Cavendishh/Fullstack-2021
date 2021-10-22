2b60d151c61400857d2c1ea07e27edcef2153eda
kavanderjanne <kavanderjanne@gmail.com>
2021-10-22 23:01:52 +0300

    13.4

--- part13/blog_list/index.js
+++ part13/blog_list/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
 
+const port = process.env.PORT
 const url = process.env.DATABASE_URL
 
 const sequelize = new Sequelize(url, {
@@ -12,19 +15,65 @@ const sequelize = new Sequelize(url, {
   },
 })
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-
-    const blogs = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT })
-    // console.log('Blogs: ', blogs)
-
-    blogs.map((b) => console.log(`${b.author}: '${b.title}', ${b.likes} likes`))
-
-    sequelize.close()
-  } catch (err) {
-    console.error('Unable to connect to the DB: ', err)
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
   }
-}
+)
 
-main()
+app.use(express.json())
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const blog = await Blog.findByPk(id)
+    if (blog) await blog.destroy()
+
+    return res.status(204).end()
+  } catch (err) {
+    return res.status(400).json({ err })
+  }
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (err) {
+    return res.status(400).json({ err })
+  }
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
