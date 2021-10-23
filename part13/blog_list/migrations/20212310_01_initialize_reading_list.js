859e46ab1e59d914693eecfd95baafd544fc7261
kavanderjanne <kavanderjanne@gmail.com>
2021-10-23 22:44:47 +0300

    13.19

---
+++ part13/blog_list/migrations/20212310_01_initialize_reading_list.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize')
+const Sequelize = require('sequelize')
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.createTable('reading_list', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      blog_id: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      is_read: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+    })
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('reading_list')
+  },
+}
---
+++ part13/blog_list/models/readingList.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize')
+const { Model, DataTypes } = require('sequelize')
+
+const { sequelize } = require('../utils/db')
+
+class ReadingList extends Model {}
+
+ReadingList.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    blog_id: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    is_read: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'reading_list',
+  }
+)
+
+module.exports = ReadingList
