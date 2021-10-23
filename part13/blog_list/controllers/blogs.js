d05f5d8497de34d3095e408a32db085b9965a392
kavanderjanne <kavanderjanne@gmail.com>
2021-10-23 17:00:54 +0300

    13.14

--- part13/blog_list/controllers/blogs.js
+++ part13/blog_list/controllers/blogs.js
@@ -5,10 +5,23 @@ const { Blog, User } = require('../models')
 const { tokenExtractor, blogFetcher, userFetcher } = require('../utils/middleware')
 
 router.get('/', async (req, res) => {
-  const where = {}
+  let where = {}
 
   if (req.query.search) {
-    where.title = { [Op.iLike]: `%${req.query.search}%` }
+    where = {
+      [Op.or]: [
+        {
+          title: {
+            [Op.iLike]: `%${req.query.search}%`,
+          },
+        },
+        {
+          author: {
+            [Op.iLike]: `%${req.query.search}%`,
+          },
+        },
+      ],
+    }
   }
 
   const blogs = await Blog.findAll({
