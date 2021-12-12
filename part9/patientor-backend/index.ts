1f2353148b4777d3864786cd28e45c07ee83aafb
kavanderjanne <kavanderjanne@gmail.com>
2021-12-12 22:38:47 +0200

    9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
