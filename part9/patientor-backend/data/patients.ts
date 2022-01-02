8f60cc165b4c6e089a586fe49752ced178004f51
kavanderjanne <kavanderjanne@gmail.com>
2022-01-02 14:30:49 +0200

    9.19

--- part9/patientor-backend/data/patients.ts
+++ part9/patientor-backend/data/patients.ts
@@ -1,7 +1,6 @@
 import { PatientEntry, EGender } from '../src/types';
-import toNewPatientEntry from '../src/utils';
 
-const data: PatientEntry[] = [
+const patients: PatientEntry[] = [
   {
     id: 'd2773336-f723-11e9-8f0b-362b9e155667',
     name: 'John McClane',
@@ -9,16 +8,44 @@ const data: PatientEntry[] = [
     ssn: '090786-122X',
     gender: EGender.Male,
     occupation: 'New york city cop',
-    entries: [],
+    entries: [
+      {
+        id: 'd811e46d-70b3-4d90-b090-4535c7cf8fb1',
+        date: '2015-01-02',
+        type: 'Hospital',
+        specialist: 'MD House',
+        diagnosisCodes: ['S62.5'],
+        description: "Healing time appr. 2 weeks. patient doesn't remember how he got the injury.",
+        discharge: {
+          date: '2015-01-16',
+          criteria: 'Thumb has healed.',
+        },
+      },
+    ],
   },
   {
     id: 'd2773598-f723-11e9-8f0b-362b9e155667',
     name: 'Martin Riggs',
     dateOfBirth: '1979-01-30',
-    ssn: '300179-77A',
+    ssn: '300179-777A',
     gender: EGender.Male,
     occupation: 'Cop',
-    entries: [],
+    entries: [
+      {
+        id: 'fcd59fa6-c4b4-4fec-ac4d-df4fe1f85f62',
+        date: '2019-08-05',
+        type: 'OccupationalHealthcare',
+        specialist: 'MD House',
+        employerName: 'HyPD',
+        diagnosisCodes: ['Z57.1', 'Z74.3', 'M51.2'],
+        description:
+          'Patient mistakenly found himself in a nuclear plant waste site without protection gear. Very minor radiation poisoning. ',
+        sickLeave: {
+          startDate: '2019-08-05',
+          endDate: '2019-08-28',
+        },
+      },
+    ],
   },
   {
     id: 'd27736ec-f723-11e9-8f0b-362b9e155667',
@@ -36,7 +63,33 @@ const data: PatientEntry[] = [
     ssn: '050174-432N',
     gender: EGender.Female,
     occupation: 'Forensic Pathologist',
-    entries: [],
+    entries: [
+      {
+        id: 'b4f4eca1-2aa7-4b13-9a18-4a5535c3c8da',
+        date: '2019-10-20',
+        specialist: 'MD House',
+        type: 'HealthCheck',
+        description: 'Yearly control visit. Cholesterol levels back to normal.',
+        healthCheckRating: 0,
+      },
+      {
+        id: 'fcd59fa6-c4b4-4fec-ac4d-df4fe1f85f62',
+        date: '2019-09-10',
+        specialist: 'MD House',
+        type: 'OccupationalHealthcare',
+        employerName: 'FBI',
+        description: 'Prescriptions renewed.',
+      },
+      {
+        id: '37be178f-a432-4ba4-aac2-f86810e36a15',
+        date: '2018-10-05',
+        specialist: 'MD House',
+        type: 'HealthCheck',
+        description:
+          'Yearly control visit. Due to high cholesterol levels recommended to eat more vegetables.',
+        healthCheckRating: 1,
+      },
+    ],
   },
   {
     id: 'd2773c6e-f723-11e9-8f0b-362b9e155667',
@@ -45,14 +98,17 @@ const data: PatientEntry[] = [
     ssn: '090471-8890',
     gender: EGender.Male,
     occupation: 'Digital evangelist',
-    entries: [],
+    entries: [
+      {
+        id: '54a8746e-34c4-4cf4-bf72-bfecd039be9a',
+        date: '2019-05-01',
+        specialist: 'Dr Byte House',
+        type: 'HealthCheck',
+        description: 'Digital overdose, very bytestatic. Otherwise healthy.',
+        healthCheckRating: 0,
+      },
+    ],
   },
 ];
 
-const patientEntries: PatientEntry[] = data.map((obj) => {
-  const object = toNewPatientEntry(obj) as PatientEntry;
-  object.id = obj.id;
-  return object;
-});
-
-export default patientEntries;
+export default patients;
--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
 
 export interface Diagnose {
   code: string;
@@ -24,3 +23,47 @@ export interface PatientEntry {
 
 export type PublicPatient = Omit<PatientEntry, 'ssn' | 'entries'>;
 export type NewPatientEntry = Omit<PatientEntry, 'id'>;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
+
+// // Define special omit for unions
+// type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// // Define Entry without the 'id' property
+// type EntryWithoutId = UnionOmit<Entry, 'id'>;
